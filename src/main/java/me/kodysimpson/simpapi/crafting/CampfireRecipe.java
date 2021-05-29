2e9e8bd31769f985907290d4437352e8ab1cc030
CyberDie22 <65131969+CyberDie22@users.noreply.github.com>
2021-05-29 16:49:12 -0700

    Crafting: Add CampfireRecipe

---
+++ src/main/java/me/kodysimpson/simpapi/crafting/CampfireRecipe.java
@@ -0,0 +1,36 @@
+package me.kodysimpson.simpapi.crafting;
+
+import me.kodysimpson.simpapi.SimpAPI;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Recipe for a Campfire
+ */
+public class CampfireRecipe extends Recipe {
+    @Override
+    public String getType() { return "Campfire"; }
+
+    private Material input;
+    private float experience;
+    private int cookingTime;
+
+    public CampfireRecipe(ItemStack result, Material input, float experience, int cookingTime) {
+        this.result = result;
+        this.input = input;
+        this.experience = experience;
+        this.cookingTime = cookingTime;
+    }
+
+    /**
+     * Creates a Bukkit recipe
+     * @param id Recipe ID
+     * @return Bukkit recipe
+     */
+    public org.bukkit.inventory.CampfireRecipe getRecipe(String id) {
+        NamespacedKey key = new NamespacedKey(SimpAPI.getInstance(), id);
+
+        return new org.bukkit.inventory.CampfireRecipe(key, result, input, experience, cookingTime);
+    }
+}
