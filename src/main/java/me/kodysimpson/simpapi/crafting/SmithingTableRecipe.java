d15e5bba21d3d5114585dc85cc04aff08db3c3d7
CyberDie22 <65131969+CyberDie22@users.noreply.github.com>
2021-05-29 16:53:07 -0700

    Crafting: Add SmithingTableRecipe

---
+++ src/main/java/me/kodysimpson/simpapi/crafting/SmithingTableRecipe.java
@@ -0,0 +1,39 @@
+package me.kodysimpson.simpapi.crafting;
+
+import me.kodysimpson.simpapi.SimpAPI;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.SmithingRecipe;
+import org.bukkit.inventory.SmokingRecipe;
+
+/**
+ * Recipe for a Smithing Table
+ */
+public class SmithingTableRecipe extends Recipe {
+    @Override
+    public String getType() { return "SmithingTable"; }
+
+    private Material input;
+    private ItemStack base;
+    private ItemStack addition;
+
+    public SmithingTableRecipe(ItemStack result, Material input, ItemStack base, ItemStack addition) {
+        this.result = result;
+        this.input = input;
+        this.base = base;
+        this.addition = addition;
+    }
+
+    /**
+     * Creates a Bukkit recipe
+     * @param id Recipe ID
+     * @return Bukkit recipe
+     */
+    public SmithingRecipe getRecipe(String id) {
+        NamespacedKey key = new NamespacedKey(SimpAPI.getInstance(), id);
+
+        return new SmithingRecipe(key, result, new RecipeChoice.ExactChoice(base), new RecipeChoice.ExactChoice(addition));
+    }
+}
