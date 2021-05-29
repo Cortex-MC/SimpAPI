3efb6bb4cab1d1d0c9ba846c7921d1700eae3fba
CyberDie22 <65131969+CyberDie22@users.noreply.github.com>
2021-05-29 16:31:15 -0700

    Add static method to get instance of SimpAPI plugin

--- src/main/java/me/kodysimpson/simpapi/SimpAPI.java
+++ src/main/java/me/kodysimpson/simpapi/SimpAPI.java
@@ -1,16 +1,26 @@
 package me.kodysimpson.simpapi;
 
+import me.kodysimpson.simpapi.crafting.CraftingManager;
+import me.kodysimpson.simpapi.crafting.CraftingTableRecipe;
+import me.kodysimpson.simpapi.exceptions.CraftingTypeNotFound;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpAPI extends JavaPlugin {
 
+    private static SimpAPI instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static SimpAPI getInstance() { return instance; }
 }
