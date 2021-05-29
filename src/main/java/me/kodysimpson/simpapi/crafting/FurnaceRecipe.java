9f97e43ffbe51499bd3052883c9ef338f3b94988
CyberDie22 <65131969+CyberDie22@users.noreply.github.com>
2021-05-29 16:45:26 -0700

    Crafting: Modify javadoc comment in CraftingManager.java

---
+++ src/main/java/me/kodysimpson/simpapi/crafting/FurnaceRecipe.java
@@ -0,0 +1,36 @@
+package me.kodysimpson.simpapi.crafting;
+
+import me.kodysimpson.simpapi.SimpAPI;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Recipe for a Furnace
+ */
+public class FurnaceRecipe extends Recipe {
+    @Override
+    public String getType() { return "Furnace"; }
+
+    private Material input;
+    private float experience;
+    private int cookingTime;
+
+    public FurnaceRecipe(ItemStack result, Material input, float experience, int cookingTime) {
+        this.result = result;
+        this.input = input;
+        this.experience = experience;
+        this.cookingTime = cookingTime;
+    }
+
+    /**
+     * Creates a Bukkit recipe
+     * @param id Recipe ID
+     * @return Bukkit recipe
+     */
+    public org.bukkit.inventory.FurnaceRecipe getRecipe(String id) {
+        NamespacedKey key = new NamespacedKey(SimpAPI.getInstance(), id);
+
+        return new org.bukkit.inventory.FurnaceRecipe(key, result, input, experience, cookingTime);
+    }
+}
