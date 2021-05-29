c5f13c476334919a7dddd5e1c6bcb7fe5b477e9a
CyberDie22 <65131969+CyberDie22@users.noreply.github.com>
2021-05-29 16:58:02 -0700

    Crafting: Add ShapelessCraftingTableRecipe

---
+++ src/main/java/me/kodysimpson/simpapi/crafting/ShapelessCraftingTableRecipe.java
@@ -0,0 +1,44 @@
+package me.kodysimpson.simpapi.crafting;
+
+import me.kodysimpson.simpapi.SimpAPI;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+
+import java.util.*;
+
+/**
+ * Shapeless Recipe for a Crafting Table
+ */
+public class ShapelessCraftingTableRecipe extends Recipe {
+    @Override
+    public String getType() { return "ShapelessCraftingTable"; }
+
+    private List<Material> items;
+
+    public ShapelessCraftingTableRecipe(ItemStack result, Material... items) {
+        this.result = result;
+        this.items = Arrays.asList(items);
+    }
+
+    /**
+     * Returns a Bukkit recipe
+     * @param id Recipe ID
+     * @return Bukkit recipe
+     */
+    public ShapelessRecipe getRecipe(String id) {
+        NamespacedKey key = new NamespacedKey(SimpAPI.getInstance(), id);
+
+        ShapelessRecipe recipe = new ShapelessRecipe(key, result);
+
+        items.forEach(recipe::addIngredient);
+
+        return recipe;
+    }
+
+    public List<Material> getItems() {
+        return new ArrayList<>(items);
+    }
+}
