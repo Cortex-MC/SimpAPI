e57d443176163c275d6f12b5f2b2a5b6a8493f99
CyberDie22 <65131969+CyberDie22@users.noreply.github.com>
2021-05-29 17:01:32 -0700

    Crafting: Register new recipe types
    Registered the following types:
     - ShapelessCraftingRecipe
     - BlastFurnaceRecipe
     - SmokerRecipe
     - CampfireRecipe
     - SmithingTableRecipe
     - StonecutterRecipe

--- src/main/java/me/kodysimpson/simpapi/crafting/CraftingManager.java
+++ src/main/java/me/kodysimpson/simpapi/crafting/CraftingManager.java
@@ -18,7 +18,13 @@ public class CraftingManager {
      */
     public static void createRecipe(Recipe recipe, String id) throws CraftingTypeNotFound {
         if (recipe instanceof CraftingTableRecipe) Bukkit.addRecipe(((CraftingTableRecipe) recipe).getRecipe(id));
+        else if (recipe instanceof ShapelessCraftingTableRecipe) Bukkit.addRecipe(((ShapelessCraftingTableRecipe) recipe).getRecipe(id));
         else if (recipe instanceof FurnaceRecipe) Bukkit.addRecipe(((FurnaceRecipe) recipe).getRecipe(id));
+        else if (recipe instanceof BlastFurnaceRecipe) Bukkit.addRecipe(((BlastFurnaceRecipe) recipe).getRecipe(id));
+        else if (recipe instanceof SmokerRecipe) Bukkit.addRecipe(((SmokerRecipe) recipe).getRecipe(id));
+        else if (recipe instanceof CampfireRecipe) Bukkit.addRecipe(((CampfireRecipe) recipe).getRecipe(id));
+        else if (recipe instanceof SmithingTableRecipe) Bukkit.addRecipe(((SmithingTableRecipe) recipe).getRecipe(id));
+        else if (recipe instanceof StonecutterRecipe) Bukkit.addRecipe(((StonecutterRecipe) recipe).getRecipe(id));
         else throw new CraftingTypeNotFound("Crafting type '" + recipe.getType() + "' not recognized");
     }
 }
