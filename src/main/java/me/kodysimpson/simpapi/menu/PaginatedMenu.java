42029c8a15506803bc817195f2f1e53f3d987d2c
Kody Simpson <kodysimpson0616@gmail.com>
2024-04-05 21:20:58 -0500

    made it so you have to pass the plugin instance when opening a menu so you can obtain it later on

--- src/main/java/me/kodysimpson/simpapi/menu/PaginatedMenu.java
+++ src/main/java/me/kodysimpson/simpapi/menu/PaginatedMenu.java
@@ -3,6 +3,7 @@ package me.kodysimpson.simpapi.menu;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -22,8 +23,8 @@ public abstract class PaginatedMenu extends Menu {
     //that the loop is on
     protected int index = 0;
 
-    public PaginatedMenu(PlayerMenuUtility playerMenuUtility) {
-        super(playerMenuUtility);
+    public PaginatedMenu(Plugin plugin, PlayerMenuUtility playerMenuUtility) {
+        super(plugin, playerMenuUtility);
     }
 
     /**
